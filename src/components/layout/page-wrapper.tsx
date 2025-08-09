6dd53c8fb911067c72cc02cab5d133d8b8ee31a6
Sebastián Jaque <jaquesebastian0@gmail.com>
2025-08-09 19:47:31 +0000

    actualizar

--- src/components/layout/page-wrapper.tsx
+++ src/components/layout/page-wrapper.tsx
@@ -15,7 +15,7 @@ export function PageWrapper({ children }: { children: React.ReactNode }) {
       const timer = setTimeout(() => {
         setLoading(false);
         sessionStorage.setItem('hasLoaded', 'true');
-      }, 4000); // 1s wait + 1s fade-out
+      }, 2000); // 1s wait + 1s fade-out
 
       return () => clearTimeout(timer);
     }
